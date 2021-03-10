dac39ecbe85c9ceaf86cbe5628b140b1181fa91c
Fabiano SantAnna <vsantanna.fabiano@gmail.com>
2021-03-10 20:00:39 -0300

    Adicione srpasta src

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+const listaUsuario = [
+    'Fabiano', 
+    'Joice',
+    'Adriano',
+    'Caique'
+]
+
+app.use(express.static(path.join(__dirname, '/src/public/')));
+
+    //Route params
+app.get('/hello/:usuario', (req, res) => {
+    const { usuario } = req.params;
+    return res.send(`Hello World ${usuario}`);
+});
+
+   //Query params
+   //Ex: localhost: 3000/usuarios?id=1
+app.get('/usuarios', (req, res) => {
+    //console.log(req.query);
+    const { nome } = req.query;
+    let listaRetorno = listaUsuario.filter(i => i.includes(nome || ''));
+    return res.json(listaRetorno);
+});
 
 
-app.get('/', (req, res) => {
-    return res.send('Hello World');
-} )
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/views/index.html'))
+})
 
-app.listen(3000, () => console.log('servidor rodando na porta'));
+app.listen(8080, () => console.log('servidor rodando na porta'));
+ 
